2a4f2ef425bd879ecfdb3da3b7e7e94485a49134
lwsrbrts <lewis@lewisroberts.com>
2015-12-20 21:13:05 +0000

    Updated to a class
    Created a class and moved Functions to a separate file.

--- PoSHue.ps1
+++ PoSHue.ps1
@@ -1,123 +1,140 @@
-# The Bridge IP address
-$Endpoint = "192.168.1.12"
+Class HueLight {
 
-# The name of the light
-$LightName = "Hue go 1"
+    ##############
+    # PROPERTIES #
+    ##############
 
-# The username created on the bridge
-$UserID = "38cbd1cbcac542f9c26ad393739b7"
-
-#############
-# FUNCTIONS #
-#############
-
-Function Get-HueLight {
-    <#
-    Returns the light number in the bridge from its friendly name - basically a search.
-    #>
-    param([parameter(Mandatory=$true)][string] $BridgeIP,
-          [parameter(Mandatory=$true)][string] $UserID,
-          [parameter(Mandatory=$true)][string] $LightFriendlyName
-    )
-    $HueData = Invoke-RestMethod -Method Get -Uri "http://$BridgeIP/api/$UserID/lights"
-
-    # Parse all light data to find the name of the light to tinker with
-    $Lights = $HueData.PSObject.Members | Where-Object {$_.MemberType -eq "NoteProperty"}
-    [string]$Light = $Lights | Where-Object {$_.Value.Name -match $LightFriendlyName}  | Select Name -ExpandProperty Name
-    Return $Light
-}
-
-Function Switch-HueLight  {
-    <#
-    Switches the state of the light.
-    If you don't specify the state (on or off) then
-    if on, turns off. If off, turns on.
-    #>
-
-    param([parameter(Mandatory=$true)][string] $BridgeIP,
-          [parameter(Mandatory=$true)][string] $UserID,
-          [parameter(Mandatory=$true)][string] $LightNumber,
-          [ValidateSet( "On", "Off" )]
-          [string[]]
-          $State
-    )
-
-    $Light = Invoke-RestMethod -Method Get -Uri "http://$BridgeIP/api/$UserID/lights/$LightNumber"
+    [string] $Light
+    [ipaddress] $BridgeIP
+    [string] $APIKey
+    [string] $JSON
+    [bool] $On
+    [ValidateRange(1,254)][int] $Brightness
+    [ValidateRange(0,65535)][int] $Hue
+    [ValidateRange(1,254)][int] $Saturation
+    [ValidateRange(153,500)][int] $ColourTemperature
     
-    # Haven't specified the state, so toggle it.
-    # Very verbose, uses the current light's state information but could just
-    # set the JSON ourselves as per Set-HueLight
-    If (!($State)) {
-        If ($Light.state.on -eq $true) {
-            $Light.state.on = $false
-            $Light.state.Psobject.Properties.Remove('colormode')
-            $Light.state.Psobject.Properties.Remove('reachable')
-            $Result = Invoke-RestMethod -Method Put -Uri "http://$BridgeIP/api/$UserID/lights/$LightNumber/state" -Body (ConvertTo-Json $Light.State)
-            Write-Output "Light should now be off."
-        }
-        Else {
-            $Light.state.on = $true
-            $Light.state.Psobject.Properties.Remove('colormode')
-            $Light.state.Psobject.Properties.Remove('reachable')
-            $Result = Invoke-RestMethod -Method Put -Uri "http://$BridgeIP/api/$UserID/lights/$LightNumber/state" -Body (ConvertTo-Json $Light.State)
-            Write-Output "Light should now be on."
-        }
-    }
-    Else {
 
+    ###############
+    # CONSTRUCTOR #
+    ###############
+
+    HueLight([string] $Name, [ipaddress] $Bridge, [string] $API) {
+        $this.BridgeIP = $Bridge
+        $this.APIKey = $API
+        $this.Light = $this.GetHueLight($Name)
+        $this.GetStatus()
+    }
+
+    ###########
+    # METHODS #
+    ###########
+
+    hidden [int] GetHueLight([string] $Name) {
+        # Change the named light in to the integer used by the bridge. We use this throughout.
+        $HueData = Invoke-RestMethod -Method Get -Uri "http://$($this.BridgeIP)/api/$($this.APIKey)/lights"
+        $Lights = $HueData.PSObject.Members | Where-Object {$_.MemberType -eq "NoteProperty"}
+        $this.Light = $Lights | Where-Object {$_.Value.Name -match $Name}  | Select Name -ExpandProperty Name
+        Return $this.Light
+    }
+
+    hidden [void] GetStatus() {
+        # Get the current values of the State, Hue, Saturation, Brightness and Colour Temperatures
+        $Status = Invoke-RestMethod -Method Get -Uri "http://$($this.BridgeIP)/api/$($this.APIKey)/lights/$($this.Light)"
+        $this.On = $Status.state.on
+        $this.Brightness = $Status.state.bri
+        $this.Hue = $Status.state.hue
+        $this.Saturation = $Status.state.sat
+        $this.ColourTemperature = $Status.state.ct
+    }
+
+    [void] SwitchHueLight() {
+        # A simple toggle. If on, turn off. If off, turn on.
+        
+        Switch ($this.On) {
+            $false  {$this.On = $true}
+            $true {$this.On = $false}
+        }
+
+        $Settings = @{}
+        $Settings.Add("on", $this.On)
+
+        $Result = Invoke-RestMethod -Method Put -Uri "http://$($this.BridgeIP)/api/$($this.APIKey)/lights/$($this.Light)/state" -Body (ConvertTo-Json $Settings)
+    }
+
+    [void] SwitchHueLight([string] $State) {
+    # Set the state of the light. Always does what you give it, irrespective of the current setting.
         Switch ($State) {
-            On  {$NewState = '{"on": true}'}
-            Off {$NewState = '{"on": false}'}
+            On  {$this.On = $true}
+            Off {$this.On = $false}
         }
-        $Result = Invoke-RestMethod -Method Put -Uri "http://$BridgeIP/api/$UserID/lights/$LightNumber/state" -Body $NewState
+
+        $Settings = @{}
+        $Settings.Add("on", $this.On)
+
+        $Result = Invoke-RestMethod -Method Put -Uri "http://$($this.BridgeIP)/api/$($this.APIKey)/lights/$($this.Light)/state" -Body (ConvertTo-Json $Settings)
     }
+
+    [void] SetHueLight([int] $Brightness, [int] $ColourTemperature) {
+        $this.Brightness = $Brightness
+        $this.ColourTemperature = $ColourTemperature
+
+        $Settings = @{}
+        $Settings.Add("bri", $this.Brightness)
+        $Settings.Add("ct", $this.ColourTemperature)
+
+        $Result = Invoke-RestMethod -Method Put -Uri "http://$($this.BridgeIP)/api/$($this.APIKey)/lights/$($this.Light)/state" -Body (ConvertTo-Json $Settings)
+    }
+
+    [void] SetHueLight([int] $Brightness, [int] $Hue, [int] $Saturation) {
+        $this.Brightness = $Brightness
+        $this.Hue = $Hue
+        $this.Saturation = $Saturation
+
+        # Feels a bit verbose to be updating our object data then re-constructing it for use... what to do...what to do...
+
+        $Settings = @{}
+        $Settings.Add("bri", $this.Brightness)
+        $Settings.Add("hue", $this.Hue)
+        $Settings.Add("sat", $this.Saturation)
+
+        $Result = Invoke-RestMethod -Method Put -Uri "http://$($this.BridgeIP)/api/$($this.APIKey)/lights/$($this.Light)/state" -Body (ConvertTo-Json $Settings)
+    }
+
 }
 
-Function Set-HueLight {
-    <#
-    Sets the light's ((Hue and Saturation levels) or (Colour Temperature)) and (Brightness), 
-    If you set the colour temperature value, Hue and Sat values are ignored (ie. Use one or the other, not both)
-    For Hue: Both 0 and 65535 are red, 25500 is green and 46920 is blue.
-    For Colour Temp: 153 is 6500K (White), 500 is 2000K (Warm)
-    #>
-    param([parameter(Mandatory=$true)][string] $BridgeIP,
-          [parameter(Mandatory=$true)][string] $UserID,
-          [parameter(Mandatory=$true)][string] $LightNumber,
-          [ValidateRange(1,254)][int] $Brightness,
-          [ValidateRange(0,65535)][int] $Hue,
-          [ValidateRange(1,254)][int] $Saturation,
-          [ValidateRange(153,500)][int] $ColourTemperature
-    )
-
-    $Settings = @{}
-
-    If ($Brightness) {
-        $Settings.Add("bri", $Brightness)
-    }
-    If ($Hue) {
-        $Settings.Add("hue", $Hue)
-    }
-    If ($Saturation) {
-        $Settings.Add("sat", $Saturation)
-    }
-    If ($ColourTemperature) {
-        $Settings.Add("ct", $ColourTemperature)
-    }
-
-
-    $Result = Invoke-RestMethod -Method Put -Uri "http://$BridgeIP/api/$UserID/lights/$LightNumber/state" -Body (ConvertTo-Json $Settings)
-
-}
 
 ##############
 # PROCESSING #
 ##############
 
-$Light = Get-HueLight -BridgeIP $Endpoint -UserID $UserID -LightFriendlyName $LightName
+# The name of the light
+$LightName = "Hue go 1"
 
-#Switch-HueLight -BridgeIP $Endpoint -UserID $UserID -LightNumber $Light -State On
+# The Bridge IP address
+$Endpoint = "192.168.1.12"
 
-# Hue Go Default Colour Temperature & Brightness
-Set-HueLight -BridgeIP $Endpoint -UserID $UserID -LightNumber $Light -ColourTemperature 370 -Brightness 50
+# The username created on the bridge
+$UserID = "38cbd1cbcac542f9c26ad393739b7"
 
-#Set-HueLight -BridgeIP $Endpoint -UserID $UserID -LightNumber $Light -Brightness 50 -Hue 25500 -Saturation 254
+# Instantiate a Hue Light Object
+$Go = [HueLight]::New($LightName, $Endpoint, $UserID)
+
+# Do stuff with the light!
+
+# Toggle it on or off
+#$Go.SwitchHueLight()
+#$Go.SwitchHueLight("On")
+#$Go.SwitchHueLight("Off")
+
+# Set the Brightness, keep the existing hue and saturation
+#$Go.SetHueLight(100, $Go.Hue, $Go.Saturation)
+
+# Set the Brightness to 50, keep the existing colour temp
+#$Go.SetHueLight(50, $Go.ColourTemperature)
+
+# Set the Brightness to 100, set colour temp to 370
+#$Go.SetHueLight(100, 370)
+
+# Just see what the object properties are.
+#$Go
